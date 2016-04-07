28398b4d437ee04d1bd462799ee4175c134cf062
unknown <Rahul.Niranjan@HC15-068.hcentive.com>
2016-04-07 14:03:11 +0530

    remove target

--- hire-internet/code/portal/job-seeker-portal/job-seeker-view/target/job-seeker-view-1.0/WEB-INF/includes/_scripts.jsp
+++
@@ -1,2 +0,0 @@
-<script type="text/javascript" src="js/angular.min.js" />
-<script type="text/javascript" src="js/job-seeker.js" />
--- hire-internet/code/portal/job-seeker-portal/job-seeker-view/target/job-seeker-view-1.0/js/job-seeker.js
+++
@@ -1,6 +0,0 @@
-var module = angular.module("homeModule",[]);
-
-module.controller("homeModuleController", ['$scope', function($scope){
-
-	$scope.testData = 'Hello World!';
-}]);
