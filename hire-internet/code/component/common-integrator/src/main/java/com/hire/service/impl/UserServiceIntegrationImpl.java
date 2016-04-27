77383f1246089580be0214428ed844562d4a08a9
harshul <harshul.varshney@gmail.com>
2016-04-27 07:37:46 +0530

    user mgmt changes

--- hire-internet/code/component/common-integrator/src/main/java/com/hire/service/impl/UserServiceIntegrationImpl.java
+++ hire-internet/code/component/common-integrator/src/main/java/com/hire/service/impl/UserServiceIntegrationImpl.java
@@ -8,7 +8,9 @@ import com.hire.service.api.UserServiceIntegration;
 import com.service.usermanagement.api.UserDataService;
 import com.service.usermanagement.domain.UserData;
 
+import ma.glasnost.orika.CustomMapper;
 import ma.glasnost.orika.MapperFactory;
+import ma.glasnost.orika.MappingContext;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
 
 @Component
@@ -27,8 +29,27 @@ public class UserServiceIntegrationImpl implements UserServiceIntegration{
 	private UserDTO mapUser(UserData user) {
 		
 		MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
-		UserDTO userDTO = mapperFactory.getMapperFacade().map(user, UserDTO.class);
 		
+		mapperFactory.classMap(UserData.class, UserDTO.class)
+			.field("userIdentifier", "userIdentifier")
+			.field("userName", "userName")
+			.field("userPassword.password", "password")
+			.field("status", "status")
+			.field("userProfile", "userProfile")
+			.customize(new CustomMapper<UserData, UserDTO>() {
+				@Override
+			    public void mapAtoB(UserData source, UserDTO target, MappingContext context) {
+			      // some mapping logic here...
+			    }
+
+			    @Override
+			    public void mapBtoA( UserDTO source, UserData target, MappingContext c) {
+			      // mapping logic here...
+			   }
+			})
+			.register();
+		
+		UserDTO userDTO = mapperFactory.getMapperFacade().map(user, UserDTO.class);
 		return userDTO;
 	}
 
